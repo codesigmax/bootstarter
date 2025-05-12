09a9d083ece854ae473cf77bd37f1f39aa27aef7
codesigmax <160916892+codesigmax@users.noreply.github.com>
2025-05-12 17:36:32 +0800

    init: 初始化项目

---
+++ src/main/java/com/qfleaf/bootstarter/BootstarterApplication.java
@@ -0,0 +1,13 @@
+package com.qfleaf.bootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstarterApplication.class, args);
+    }
+
+}
