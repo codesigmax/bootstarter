8f7345f7f6f3d379a36a27ff2bcf0a2ee9ab9457
codesigmax <160916892+codesigmax@users.noreply.github.com>
2025-05-16 21:24:35 +0800

    feat: 新增mapper扫描包路径

--- src/main/java/com/qfleaf/bootstarter/BootstarterApplication.java
+++ src/main/java/com/qfleaf/bootstarter/BootstarterApplication.java
@@ -1,9 +1,11 @@
 package com.qfleaf.bootstarter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qfleaf.bootstarter.dao.mapper")
 public class BootstarterApplication {
 
     public static void main(String[] args) {
