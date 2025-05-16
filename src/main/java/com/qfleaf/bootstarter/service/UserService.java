5db37125cb8597bde6bf1f9e88cb86bade5225a7
codesigmax <160916892+codesigmax@users.noreply.github.com>
2025-05-16 21:26:43 +0800

    feat: 实现登陆服务和注册服务

---
+++ src/main/java/com/qfleaf/bootstarter/service/UserService.java
@@ -0,0 +1,13 @@
+package com.qfleaf.bootstarter.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.qfleaf.bootstarter.model.User;
+import com.qfleaf.bootstarter.model.request.RegisterRequest;
+import com.qfleaf.bootstarter.security.request.UnifiedLoginRequest;
+import com.qfleaf.bootstarter.security.response.TokenLoginResponse;
+
+public interface UserService extends IService<User> {
+    boolean register(RegisterRequest registerRequest);
+
+    TokenLoginResponse login(UnifiedLoginRequest unifiedLoginRequest);
+}
---
+++ src/main/java/com/qfleaf/bootstarter/service/impl/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.qfleaf.bootstarter.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.qfleaf.bootstarter.common.ResultCode;
+import com.qfleaf.bootstarter.common.exception.BusinessException;
+import com.qfleaf.bootstarter.dao.UserDao;
+import com.qfleaf.bootstarter.dao.mapper.UserMapper;
+import com.qfleaf.bootstarter.model.User;
+import com.qfleaf.bootstarter.model.converter.UserConverter;
+import com.qfleaf.bootstarter.model.request.RegisterRequest;
+import com.qfleaf.bootstarter.security.authentication.token.factory.AuthenticationTokenFactory;
+import com.qfleaf.bootstarter.security.request.UnifiedLoginRequest;
+import com.qfleaf.bootstarter.security.response.TokenLoginResponse;
+import com.qfleaf.bootstarter.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+    private final AuthenticationManager authenticationManager;
+    private final AuthenticationTokenFactory authenticationTokenFactory;
+    private final UserDao userDao;
+    private final UserConverter userConverter;
+
+    @Override
+    public boolean register(RegisterRequest registerRequest) {
+        UserDao.ExistType exist = userDao.exist(registerRequest);
+        if (exist != null) {
+            throw new BusinessException(ResultCode.CONFLICT.getCode(), exist.getMsg());
+        }
+        User entity = userConverter.toEntity(registerRequest);
+        return userDao.save(entity);
+    }
+
+    @Override
+    public TokenLoginResponse login(UnifiedLoginRequest unifiedLoginRequest) {
+        Authentication token = authenticationTokenFactory.createAuthenticatedToken(unifiedLoginRequest);
+        Authentication authenticate = authenticationManager.authenticate(token);
+        UserDetails principal = (UserDetails) authenticate.getPrincipal();
+        // jwt存储到redis
+        return userDao.cacheToken(principal);
+    }
+}
